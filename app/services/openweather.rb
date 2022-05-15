e0a73b1919baaaa8d9ac10149c17764dba5664e3
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2022-05-15 21:00:06 +0100

    fix: revert to original

--- app/services/openweather.rb
+++
@@ -1,7 +0,0 @@
-module Openweather
-  class Search
-    def self.by_location(location)
-      Faraday.get 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + ENV['API_KEY'] + '&units=metric'
-    end
-  end
-end
